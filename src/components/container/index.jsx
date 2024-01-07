89ef4bced32460253ca2f47e448d8770a2c5dc80
Developer_Tarak <taraksiddhanta1@gmail.com>
2024-01-07 17:40:55 +0530

    Container

---
+++ src/components/container/index.jsx
@@ -0,0 +1,15 @@
+export default function Container() {
+    return (
+        <div>
+
+            <div id="Container" style={{ width: '75%', height: 'auto' }}>
+                <div id="Container-body">
+                    <h4 id="Container-title">Comtainer title</h4>
+                    <p id="Container-text">Some example text. Some example text.</p>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
