0aa799b6da7128f2274d5950ed38f8fab41ce05c
Tarak Siddhanta <155563311+taraksiddhanta1@users.noreply.github.com>
2024-01-08 21:24:57 +0530

    Update index.jsx

--- src/components/mobile/index.jsx
+++ src/components/mobile/index.jsx
@@ -2,7 +2,7 @@ export default function Mobile() {
     return (
         <div>
             <label for="phone">Enter a mobile number:</label><br /><br />
-            <input type="mobile" id="phone" name="phone" placeholder="+91 00000 00000" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required /><br /><br />
+            <input type="tel" id="phone" name="phone" placeholder="+91 00000 00000" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required /><br /><br />
             <br /><br />
             <input type="submit" />
         </div>
